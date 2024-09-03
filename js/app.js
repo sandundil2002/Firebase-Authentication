80d993273eaafedc7c37cf63b86b6f277d1000fd
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-09-03 23:40:00 +0530

    firebase configurations implemented.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("Check");
+const firebaseConfig = {
+  apiKey: "AIzaSyAa3LZqiglLwOcM9Z4OuDbhZkNoSQWr-ZI",
+  authDomain: "fir-authentication-69c5e.firebaseapp.com",
+  projectId: "fir-authentication-69c5e",
+  storageBucket: "fir-authentication-69c5e.appspot.com",
+  messagingSenderId: "299004620336",
+  appId: "1:299004620336:web:718713f1beabb1588047f5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//create a auth instance
+const auth = firebase.auth();
