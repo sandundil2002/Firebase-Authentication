b645f7dfcfc8a317eb1e55633ee355e434e2bca1
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-09-04 00:12:36 +0530

    import Firebase modules and add user signup functionality.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAa3LZqiglLwOcM9Z4OuDbhZkNoSQWr-ZI",
   authDomain: "fir-authentication-69c5e.firebaseapp.com",
@@ -10,5 +19,22 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-//create a auth instance
-const auth = firebase.auth();
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+//signup function
+document.getElementById("btn-signup").addEventListener("click", (e) => {
+  const email = document.getElementById("sign-email").value;
+  const password = document.getElementById("sign-password").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("User created");
+      document.getElementById("login-btn").click();
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+});
